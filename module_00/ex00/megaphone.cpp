caaf731c6b5769df2d4cb06319293a0c4590ec5c
kwevan <kyg972@msn.com>
2020-12-11 18:41:25 +0100

    day 00 ex 00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void	ft_print(std::string str)
+{
+	for (int i = 0; str[i]; i++)
+		std::cout << (char)toupper(str[i]);
+}
+
+int		main(int ac, char **av)
+{
+	std::string no_args_str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+
+	if (ac == 1)
+		ft_print(no_args_str);
+	for (int i = 1; av[i]; i++)
+	{
+		ft_print(av[i]);
+		ft_print(av[i + 1] ? " " : "");
+	}
+	std::cout <<  std::endl;
+	return (0);
+}
