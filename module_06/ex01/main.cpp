f956565dae337282cf094514dd76e226f5a46124
kwevan <kyg972@msn.com>
2021-02-02 22:05:54 +0100

    mod06 ready for corr

--- module_06/ex01/main.cpp
+++ module_06/ex01/main.cpp
@@ -1,21 +1,66 @@
 #include <iostream>
 #include <cstdlib>
+#include <array>
 
-#include <unistd.h>
+
+typedef struct	s_data
+{
+	char s1[8];
+	int			i;
+	char s2[8];
+}				t_data;
+
+char random_char()
+{
+	std::string s = "0123456789abcdefghijklmnopqrstuvwxyz";
+	return (s[rand() % 36]);
+}
+
+void *serialize()
+{
+	t_data *data = new t_data;
+	data->i = rand() % INT_MAX;
+	for (int i = 0; i < 8; i++)
+	{
+		data->s1[i] = random_char();
+		data->s2[i] = random_char();
+	}
+	return reinterpret_cast<void *>(data);
+}
+
+t_data *deserialize(void *value) {
+
+	t_data *data = reinterpret_cast<t_data *>(value);
+
+	return data;
+}
+
+void printArray(char s[8])
+{
+	for (int i = 0; i < 8 ; i++)
+		std::cout << s[i];
+	std::cout << std::endl;
+}
 
 int main()
 {
-	int random;
-
 	srand(time(NULL));
-	while ((random = rand()) > 10000)
-	{
-	
-		std::cout << " .?. ";
-		usleep(200);
 
-		if (random >= 0 && random <= 300000)
-			std::cout << "that's how we're going to understand the exercise, yes, well done !";
-	}
-	
+	void *ptr = serialize();
+
+
+	s_data *data = deserialize(ptr);
+
+	std::cout << "Deserialized data: \n\n";
+
+	std::cout << "str1 : ";
+	printArray(data->s1);
+	std::cout << "int  : "<< data->i << "\n";
+	std::cout << "str2 : ";
+	printArray(data->s2);
+
+
+	delete data;
+
+	return (0);	
 }
--- module_06/ex02/main.cpp
+++ module_06/ex02/main.cpp
@@ -1,5 +1,68 @@
-#include <iostream>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
+void identify_from_pointer(Base * p)
+{
+	if(dynamic_cast<A*>(p))
+    	std::cout << "A" << std::endl;
+	else if(dynamic_cast<B*>(p))
+    	std::cout << "B" << std::endl;
+	else if(dynamic_cast<C*>(p))
+    	std::cout << "C" << std::endl;
+}
+
+void identify_from_reference(Base &p)
+{
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception &e) {}
+
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception &e) {}
+
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception &e) {}
+}
 
 int main()
-{}
+{
+	A *a = new A;
+	B *b = new B;
+	C *c = new C;
+
+	Base *n = new Base;
+	
+	identify_from_pointer(a);
+	identify_from_pointer(b);
+	identify_from_pointer(c);
+	identify_from_pointer(n);
+	identify_from_pointer(NULL);
+
+	std::cout << "\n ------- \n\n";
+
+	identify_from_reference(*a);
+	identify_from_reference(*b);
+	identify_from_reference(*c);
+	identify_from_reference(*n);
+
+	delete a;
+	delete b;
+	delete c;
+	delete n;
+	
+}
