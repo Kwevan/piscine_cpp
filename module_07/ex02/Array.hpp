8cc672f5980f91c41ead1b0e9a6387c0131364d3
kwevan <kyg972@msn.com>
2021-02-04 01:29:51 +0100

    mod07 ok - need to test on VM

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -7,11 +7,72 @@ template <typename T>
 class Array
 {
 	public:
-		Array() {array = NULL;}
-		~Array() { delete []array;}
-		int size(){return 1;}
+		Array<T>()
+		{
+			array = NULL;
+			mySize = 0;
+		}
+		Array<T>(unsigned int n)
+		{
+			array = new T[n];
+			mySize = n;
+		}
+
+		~Array<T>()
+		{
+			if (array)	
+			delete []array;
+		}
+
+		unsigned int size() const {return mySize;};
+
+		 T  &operator[] (unsigned int index) const
+		{
+			if (!mySize || index >= mySize)
+				throw OutOfBoundsException();
+			
+			return (array[index]);
+		}
+		Array<T>(const Array<T> &copy)
+		{
+			array = NULL;
+			mySize = 0;
+			*this = copy;
+		}
+
+	 const Array<T> &operator=(const Array<T> &copy)
+	{
+		if (array)
+			delete[] array;
+		mySize = copy.size();
+			if (mySize)
+			{
+				array = new T[mySize]();
+				for (unsigned int i = 0; i < mySize; i++)
+					array[i] = copy.array[i];
+			}
+		return (*this);
+		};
+
+		class OutOfBoundsException : public std::exception
+		{
+			public:
+			virtual	const char* what() const throw()
+			{
+				return ("Out of bounds");
+			}
+		};
+	void print()
+	{
+		if (mySize)
+			for (unsigned int i = 0; i < mySize; i++)
+				std::cout << "[ " << array[i] << " ] " << "\n";
+		std::cout << "\n";
+	}
+
 	private:
 			T *array;
+			unsigned int mySize;
 };
 
 #endif
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -2,6 +2,44 @@
 
 int main()
 {
-    Array<int> arr_int = Array<int>();
-	std::cout << arr_int.size();
+    Array<int> emptyArr = Array<int>(2);
+	std::cout << "size of emptyArr is: " << emptyArr.size()
+	<< "\n\n";
+    
+	Array<int> arrInt = Array<int>(2);
+
+	try
+	{
+		std::cout << "trying to access out of bounds element of arrInt";
+		std::cout << arrInt[10];
+	}
+	catch(std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << "\n\n";
+	}
+    Array<double> arrDouble = Array<double>(4);
+	arrDouble[0] = 5;
+	std::cout << "size of arrDouble is: " << arrDouble.size() << std::endl;
+	std::cout << "arrDouble[0] is: " << arrDouble[0] << "\n\n";
+
+
+
+	Array<int> arr = Array<int>(2);
+
+	arr[0] = 444;
+	arr[1] = 555;
+	std:: cout << "array 1 [ before ] modification of array 2:\n";
+	arr.print();
+	Array<int> arr2 = arr;
+
+	std:: cout << "array 2 [ before ] modification of himself:\n";
+	arr2.print();
+
+	arr2[0] = 91;
+	arr2[1] = 92;
+	
+	std:: cout << "array 1 [ after ] modification of array2:\n";
+	arr.print();
+	std:: cout << "array 2 [ after ] modification of himself:\n";
+	arr2.print();
 }
